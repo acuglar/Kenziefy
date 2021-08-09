2e3d3db28fd49289afe8306069a3c42444c9c443
Alexandre Uglar <acuglar@gmail.com>
2021-08-09 03:57:40 -0300

    views

--- songs/serializers.py
+++ songs/serializers.py
@@ -1,35 +1,23 @@
 from rest_framework import serializers
 from collections import OrderedDict
 
+
 class SampleSerializer(serializers.Serializer):
     name = serializers.CharField()
     age = serializers.IntegerField(required=True)
 
+
 class ArtistSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)  # wo read_only -> request: "this field is required"
-    name = serializers.CharField()
-    formed_in = serializers.IntegerField()  # input default 2021 foi colocado manualmente em migrate 
-    status = serializers.CharField()  # input default 'Active' foi colocado manualmente em migrate
+    id = serializers.IntegerField(read_only=True) 
+    name = serializers.CharField(required=False)
+    formed_in = serializers.IntegerField(required=False)  
+    status = serializers.CharField(required=False)  
+
 
 class SongSerializer(serializers.Serializer):
     title = serializers.CharField()
-    # artist_id = serializers.IntegerField()
-    # artist = serializers.CharField() 
-    """
-    [
-        {
-            "title": "Holy Diver",
-            "artist_id": 1,
-            "artist": "Artist object (1)" # __str__ => "artist": "Dio"
-        },
-        ...
-    ]
-    """
-    # Essa não é a melhor forma de trabalhar view de relacionamentos.
-    # Usar os metódos de serializers para relacionamento na ref artist
-    # artist = serializers.PrimaryKeyRelatedField(read_only=True)  # id. artist_id
-    # artist = serializers.StringRelatedField(read_only=True)  # invoca __str__
     artist = ArtistSerializer() 
+
 
 class SongSimpleSerializer(serializers.Serializer):    
     title = serializers.CharField()
@@ -38,15 +26,17 @@ class SongSimpleSerializer(serializers.Serializer):
 class ArtistSongsSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
-    formed_in = serializers.IntegerField(write_only=True)
+    formed_in = serializers.IntegerField()
     status = serializers.CharField()
-    musics = SongSerializer(many=True, read_only=True, source='songs')
-    # source indica qual atributo sera buscado o valor, neste caso songs por musics
-    
+    musics = SongSimpleSerializer(many=True, source='songs')  
     total_songs = serializers.SerializerMethodField()
-    #  SerializerMethodField indica via método 
     
     def get_total_songs(self, obj):
         if (isinstance(obj, OrderedDict)):
             return 0
         return { 'count': obj.songs.count()}
+
+
+class PlaylistSerializer(serializers.Serializer):
+    title = serializers.CharField()
+    songs = SongSerializer(many=True)
--- songs/urls.py
+++ songs/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import SampleView, ParamView, KenziefyArtistView, KenziefySongView
+from .views import SampleView, ParamView, ArtistView, ArtistDetailView, PlaylistView
 
 urlpatterns = [
     path('sample/', SampleView.as_view()),
     path('sample/<str:name>/', ParamView.as_view()),
-    path('kenziefy/', KenziefyArtistView.as_view()),
-    path('kenziefy/songs/', KenziefySongView.as_view()),
+    path('artists/', ArtistView.as_view()),
+    path('artists/<int:artist_id>', ArtistDetailView.as_view()),
+    path('playlists/', PlaylistView.as_view())
 ]
+
